7711f0f9123c1ed269728861217cc11ff220821c
unknown <1910478300069@faculdade.edu>
2022-06-27 19:36:38 -0300

    .

---
+++ aula_20_06_22/src/main/java/AulaFrutas/Produto.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AulaFrutas;
+
+/**
+ *
+ * @author 1910478300069
+ */
+public class Produto {
+  
+  private String nome ;
+  private double preco;
+  
+  public Produto(String nome, double preco){
+        
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+ 
+}
+
